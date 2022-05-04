3b66bfb963ba224d4c118695d8293c30fe6eff7c
overthestream <njhnjh02@naver.com>
2022-05-04 19:59:22 +0900

    initial commit

---
+++ src/GlobalStyle.js
@@ -0,0 +1,56 @@
+import { createGlobalStyle } from 'styled-components';
+import './fonts/font.css';
+
+const GlobalStyle = createGlobalStyle`
+
+  html {
+    font-family: 'KU' ;
+    font-size: 16px;
+    height: 100%;
+  }
+
+  body {
+    min-height: 100%;
+    background: #ffffff;
+    margin: 0;
+    padding: 0;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+
+    scrollbar-width: none;
+  }
+
+  textarea, button, button:active {
+    resize: none;
+    outline: none;
+    border: none;
+  }
+
+  a, a:visited {
+    text-decoration: none;
+  }
+
+  input, textarea {
+    border: none;
+
+    :focus {
+      outline: none;
+    }
+  }
+
+  * { 
+    box-sizing: border-box;
+    margin: 0;
+
+    ::selection {
+      background: #343a40;
+      color: #f1f3f5;
+    }
+  }
+
+  #root {
+    overflow-y: visible;
+  }
+`;
+
+export default GlobalStyle;
